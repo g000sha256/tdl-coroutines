ea06b5a0078b6b4eb1107666c97514a5b53a5a65
Georgii Ippolitov <detmmpmznb@g000sha256.dev>
2026-01-09 20:44:42 +0200

    Update copyright year

--- tdl-coroutines/src/commonMainGenerated/kotlin/dev/g000sha256/tdl/function/SummarizeMessage.kt
+++ tdl-coroutines/src/commonMainGenerated/kotlin/dev/g000sha256/tdl/function/SummarizeMessage.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Georgii Ippolitov (g000sha256)
+ * Copyright 2026 Georgii Ippolitov (g000sha256)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
