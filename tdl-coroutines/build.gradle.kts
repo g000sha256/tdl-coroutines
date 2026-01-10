2ea8d79516920a4430d25ac889bd42aaa41d2955
Georgii Ippolitov <detmmpmznb@g000sha256.dev>
2026-01-10 19:41:41 +0200

    Version 8.0.0

--- tdl-coroutines/build.gradle.kts
+++ tdl-coroutines/build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinOnlyTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinTargetWithBinaries
 
 group = "dev.g000sha256"
-version = "7.0.0"
+version = "8.0.0"
 
 plugins {
     alias(notation = catalog.plugins.android.library)
